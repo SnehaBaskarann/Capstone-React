5287701704f8e6c9ab8b36c69e867ba55249e799
Nithyasubraman <nithyasubramanian0527@gmail.com>
2024-05-02 14:53:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './Home'
+import FileUploader from './BulkUpload';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="FileUploader" element={<FileUploader/>} />
         {/* <Route path="/quizengine" element={<QuizEngine/>}/>
         <Route path="/addquestion" element={<AdminAddQuestions/>}/> */}
       </Routes>
