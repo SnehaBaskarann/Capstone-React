25d760787e97561da01ebd11684c9228f26c911a
Sneha Baskaran <sneha.baskaran@relevantz.com>
2024-05-03 14:55:46 +0530

    added addquestion route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './Home'
 import FileUploader from './BulkUpload';
 import UploadBulkQuiz from './components/UploadBulkQuiz';
-
+import AddQuestion from './components/AddQuestions';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="FileUploader" element={<FileUploader/>} />
         <Route path="bulkQuiz" element={<UploadBulkQuiz/>} />
+        <Route path="addQuestion" element={<AddQuestion/>} />
         {/* <Route path="/quizengine" element={<QuizEngine/>}/>
         <Route path="/addquestion" element={<AdminAddQuestions/>}/> */}
       </Routes>
