80da03d5964d3e4f3533d7fe36eec7230100d08d
Nithyasubraman <nithyasubramanian0527@gmail.com>
2024-05-04 17:14:51 +0530

    check

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './Home'
 import FileUploader from './BulkUpload';
+import QuizEditor from './components/PreviewQuiz';
+import QuizManager from './components/QuizManager';
+import CombineQuizEditor from './components/CombineQuizEditor';
+import FetchApi from './components/FetchApi';
 
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="FileUploader" element={<FileUploader/>} />
+        <Route path="quizEditor" element={<QuizEditor/>} />
+        <Route path="quizManager" element={<QuizManager/>} />
+        <Route path="combineQuizEditor" element={<CombineQuizEditor/>} />
+        <Route path="fetchApi" element={<FetchApi/>} />
         {/* <Route path="/quizengine" element={<QuizEngine/>}/>
         <Route path="/addquestion" element={<AdminAddQuestions/>}/> */}
       </Routes>
